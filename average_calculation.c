99086a7d487fbc9c1185c95af916b280f62b69c1
Beril Bulut <buluttberill@gmail.com>
2026-03-16 09:39:58 +0300

    Update average_calculation.c

--- average_calculation.c
+++ average_calculation.c
@@ -1,38 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    int ogrenci_sayisi;
+    float vize, final, ortalama;
 
-    printf("Kac ogrenci girilecek: ");
-    scanf("%d", &ogrenci_sayisi);
+    printf("Vize notunu gir: ");
+    scanf("%f", &vize);
+    printf("Final notunu gir: ");
+    scanf("%f", &final);
 
-    float vizeler[ogrenci_sayisi];
-    float finaller[ogrenci_sayisi];
-    float ortalamalar[ogrenci_sayisi];
-    float sinif_toplami = 0;
+    ortalama = (vize * 0.4) + (final * 0.6);
 
-    for (int i = 0; i < ogrenci_sayisi; i++) {
-        printf("\n%d. ogrenci vize notu: ", i + 1);
-        scanf("%f", &vizeler[i]);
-        printf("%d. ogrenci final notu: ", i + 1);
-        scanf("%f", &finaller[i]);
+    printf("\nOrtalamaniz: %.2f\n", ortalama);
+    printf("Durum: ");
 
-        ortalamalar[i] = (vizeler[i] * 0.4) + (finaller[i] * 0.6);
-        sinif_toplami += ortalamalar[i];
+    if (ortalama >= 60) {
+        printf("GECTI\n");
+    } 
+    else if (ortalama >= 50) {
+        printf("SARTLI GECTI\n");
+    } 
+    else {
+        printf("KALDI\n");
     }
-
-    printf("\n--- SONUCLAR ---\n");
-    printf("No\tVize\tFinal\tOrtalama\tDurum\n");
-    printf("--------------------------------------------\n");
-
-    for (int i = 0; i < ogrenci_sayisi; i++) {
-        printf("%d\t%.1f\t%.1f\t%.1f\t\t%s\n", 
-               i + 1, vizeler[i], finaller[i], ortalamalar[i], 
-               (ortalamalar[i] >= 50 ? "GECTI" : "KALDI"));
-    }
-
-    printf("--------------------------------------------\n");
-    printf("Sinif Ortalamasi: %.2f\n", sinif_toplami / ogrenci_sayisi);
 
     return 0;
 }
